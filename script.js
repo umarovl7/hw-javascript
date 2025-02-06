afff05d54969fa8abf57ac31706c0d34a9fb2b67
umarovl7 <azamatumarov770@gmail.com>
2025-02-06 19:22:55 +0500

    done

---
+++ script.js
@@ -0,0 +1,10 @@
+const country = {
+    name: "Spain",
+    capital: "Marid",
+    area: 506.030,
+    population: 48.37
+};
+
+document.getElementById("Homework").innerHTML =
+  +"According to statistics in 2023" + country.name +  "'s population is " + country.population + " million. ";
+
