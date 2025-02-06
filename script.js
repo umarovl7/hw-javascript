08ae9256fbd14fd168eb1a0f587d4ad3a2062728
umarovl7 <azamatumarov770@gmail.com>
2025-02-06 19:35:59 +0500

    done

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-const country = {
-    name: "Spain",
-    capital: "Marid",
-    area: 506.030,
-    population: 48.37
-};
+// const country = {
+   // name: "Spain",
+   // capital: "Marid",
+   // area: 506.030,
+   // population: 48.37
+// };
 
+// document.getElementById("Homework").innerHTML =
+// country.name +  "'s population is " + country.population + " million. ";
+
+
+const footballclub = {
+    name: "Barcelona",
+    founded: 1899,
+    stadium: "Camp Nou",
+    nicknames: "Barça", 
+    location: "Spain"
+};
 document.getElementById("Homework").innerHTML =
-  +"According to statistics in 2023" + country.name +  "'s population is " + country.population + " million. ";
+footballclub.name + "'s nickname is " + footballclub.nicknames + ".";
 
